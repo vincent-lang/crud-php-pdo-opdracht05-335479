9156b99cb03a1019b82a5cff68772b66aa345faf
vincent van de merwe <335479@student.mboutrecht.nl>
2023-02-27 19:40:35 +0100

    third commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +9,7 @@
     <link rel="shortcut icon" href="img/anon.png" type="image/x-icon">
     <title>BASIC-FIT Utrecht</title>
 </head>
+
 <body>
     <h1>BASIC-FIT Utrecht</h1>
     <form action="create.php" method="post">
@@ -15,30 +17,31 @@
         <select name="" id="" required>
             <option value=""></option>
         </select>
-        <label for="">Selecteer een lidmaatschap:</label>
-        <input type="radio" name="Comfort" id="Comfort" required>Comfort
-        <input type="radio" name="Premium" id="Premium" required>Premium
-        <input type="radio" name="All_in" id="All_in" required>All in
         <br>
-        <label for="tel">Uw telefoonnummer:</label>
-        <input type="tel" name="tel" id="tel" pattern=".{3,16}" placeholder="+31 6 30694820" required>
+        <label for="1">Selecteer een lidmaatschap:</label>
+        <input type="radio" name="1" id="comfort" required>Comfort
+        <input type="radio" name="1" id="premium" required>Premium
+        <input type="radio" name="1" id="all_in" required>All in
         <br>
-        <label for="email">Uw e-mailadres:</label>
-        <input type="email" name="email" id="email" placeholder="randomperson@example.com" required>
         <br>
-        <label for="datum">Afspraak datum:</label>
-        <input type="datetime-local" name="datum" id="datum" required>
+        <label for="1">Looptijd:</label>
+        <input type="radio" name="1" id="jaarlidmaatschap" required>Jaarlidmaatschap
+        <input type="radio" name="1" id="flex_optie" required>Flex optie
         <br>
-        <label for="nagelbijt">Nagelbijt arrangement (termijnbetaling mogelijk) €180</label>
-        <input type="checkbox" name="nagelbijt" id="nagelbijt">
-        <label for="luxemanicure">Luxe manicure (massage en handpakking) €30,00</label>
-        <input type="checkbox" name="luxemanicure" id="luxemanicure">
-        <label for="nagelreparatie">Nagelreparatie per nagel (in eerste week gratis) €5,00</label>
-        <input type="checkbox" name="nagelreparatie" id="nagelreparatie">
-            <input id="submit" type="submit" value="Sla op">
-            <input id="reset" type="reset" value="reset">
-            <input type="hidden" name="hidden" value="datetime-local">
+        <br>
+        <label for="">Selecteer je extra's:</label>
+        <input type="checkbox" name="sportswater" id="sportswater">Yanga sportswater € 2,50 per 4 weken
+        <input type="checkbox" name="coach" id="coach">Personal online coach € 60,00 eenmalig
+        <input type="checkbox" name="intro" id="intro">Personal training intro € 25,00 eenmalig
+        <br>
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" id="email">
+        <br>
+        <input id="submit" type="submit" value="Sla op">
+        <input id="reset" type="reset" value="reset">
+        <input type="hidden" name="hidden" value="datetime-local">
         </fieldset>
     </form>
 </body>
+
 </html>
